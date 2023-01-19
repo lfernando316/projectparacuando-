f37c8e4815b7a82be2ed82dd3e457233b3f073c2
lfernando316 <luiz_fernando316@hotmail.com>
2023-01-19 11:44:11 -0500

    feat: add routes and layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import Footer from './Footer';
 import Header from './Header';
 
-export default function Layout({ children }) {
+export default function layout({ children }) {
   return (
     <>
       <Header />
