f5c99c1dc5b06a8aa44218a5cad682df4c383000
Niumar Zerpa <niumarzerpa86@gmail.com>
2023-01-20 21:32:55 -0500

    feat: tarea Configuración de variables / estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,17 @@ module.exports = {
       lg: '1200px',
       xl: '1536px',
     },
+    colors: {
+      blue: '#1B4DB1',
+      yellow: '#F3F243',
+      pink: '#FF64BC',
+      black: '#000000',
+      blackLigth: '#1A1E2E',
+      gray: '#A7A6A7',
+      grayLight: '#D9D9D9',
+      grayLighter: '#F8F7FA',
+      red: '#EF3F47',
+    },
   },
   plugins: [],
 };
